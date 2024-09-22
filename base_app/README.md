05e8c4be2053c0a917c939451ec886e509f6e01e
Sahil Agarwal <sahildagarwal@gmail.com>
2024-09-22 15:10:48 -0700

    create base chainlit app with history and rag

--- base_app/README.md
+++ base_app/README.md
@@ -2,10 +2,14 @@
 ```
 python3 -m venv .venv
 source .venv/bin/activate
-pip install python-dotenv llama-index langfuse
+pip install python-dotenv langsmith chainlit openai llama_index
 ```
 
 ## Run the app
 ```
 chainlit run app.py -w
 ```
+
+
+## What is this?
+This is base app that has chainlit set up with history, talking with Open AI, and also reads all docs in the `data/` directory and adds it to the context with user querying.
--- base_app/app.py
+++ base_app/app.py
@@ -1,9 +1,11 @@
-import chainlit as cl
-import openai
 import os
 from dotenv import load_dotenv
+import chainlit as cl
+import openai
 from langsmith.wrappers import wrap_openai
 from langsmith import traceable
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
+
 from prompts import SYSTEM_PROMPT
 
 # Load environment variables
@@ -35,6 +37,25 @@ gen_kwargs = {
 # Configuration setting to enable or disable the system prompt
 ENABLE_SYSTEM_PROMPT = True
 
+retriever = None
+index_location = './data_index/'
+
+@cl.on_chat_start
+async def start_main():
+    if os.path.exists(index_location):
+        # Load dataset from local file if it exists
+        storage_context = StorageContext.from_defaults(persist_dir=index_location)
+        # load index
+        index = load_index_from_storage(storage_context)
+    else:
+        # Generate dataset if local file doesn't exist
+        # Load documents from a directory (you can change this path as needed)
+        documents = SimpleDirectoryReader("data").load_data()
+        index = VectorStoreIndex.from_documents(documents)
+        index.storage_context.persist(persist_dir=index_location)
+    global retriever
+    retriever = index.as_retriever(retrieval_mode='similarity', k=3)
+
 @traceable
 @cl.on_message
 async def on_message(message: cl.Message):
@@ -44,6 +65,17 @@ async def on_message(message: cl.Message):
     if ENABLE_SYSTEM_PROMPT and (not message_history or message_history[0].get("role") != "system"):
         system_prompt_content = SYSTEM_PROMPT
         message_history.insert(0, {"role": "system", "content": system_prompt_content})
+
+    # get relevant docs from rag/index
+    relevant_docs = retriever.retrieve(message.content)
+    doc_content = ""
+    for i, doc in enumerate(relevant_docs):
+        doc_content += doc.node.get_content()
+    if len(doc_content) > 0:
+        # if previous content is present, remove it
+        if len(message_history) > 2 and message_history[1].get("role") == "system":
+            message_history.pop(1)
+        message_history.insert(1, {"role": "system", "content": doc_content})
 
     message_history.append({"role": "user", "content": message.content})
 
